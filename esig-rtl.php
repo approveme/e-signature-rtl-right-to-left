6615dd8fe144201d687d7956cf48d1785249e9c7
KMG <billing@approveme.com>
2017-10-09 10:07:23 -0700

    Updated version to 1.5.2.0

--- esig-rtl.php
+++ esig-rtl.php
@@ -7,9 +7,9 @@
  * Plugin Name:       WP E-Signature - RTL
  * Plugin URI:        http://approveme.me/wp-digital-e-signature
  * Description:       Adds Right-to-left (RTL) support to Agreement page & Admin Area of E-signature.
- * Version:           1.4.0
+ * Version:           1.5.2.0
  * Author:            Approve Me
- * Author URI:        http://approveme.me/
+ * Author URI:        https://www.approveme.com/
  */
 // If this file is called directly, abort.
 if (!defined('WPINC')) {
